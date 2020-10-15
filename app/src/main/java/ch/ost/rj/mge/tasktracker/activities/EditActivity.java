2a16116120e853bd54dcbd22f370bf6c1bf91dae
pemmeneg <philipp@emmenegger-mail.ch>
2020-10-15 11:10:27 +0200

    Created Project

---
+++ app/src/main/java/ch/ost/rj/mge/tasktracker/activities/EditActivity.java
@@ -0,0 +1,15 @@
+package ch.ost.rj.mge.tasktracker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import ch.ost.rj.mge.tasktracker.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
