cf665e5c92744e8ccf6fdaf401d4551836e58041
Jojobob97 <joel@schaltegger.me>
2020-10-15 12:47:02 +0200

    started on detail activity

--- app/src/main/java/ch/ost/rj/mge/tasktracker/activities/DetailActivity.java
+++ app/src/main/java/ch/ost/rj/mge/tasktracker/activities/DetailActivity.java
@@ -2,14 +2,23 @@ package ch.ost.rj.mge.tasktracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import ch.ost.rj.mge.tasktracker.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    public static Intent createIntent(Context context) {
+        Intent intent = new Intent(context, EditActivity.class);
+        return intent;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
     }
 }
