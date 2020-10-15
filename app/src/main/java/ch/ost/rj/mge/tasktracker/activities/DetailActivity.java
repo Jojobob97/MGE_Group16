c30b1e8d831f257b5ca796231474f4ce8a85b912
Jojobob97 <joel@schaltegger.me>
2020-10-15 13:58:28 +0200

    started on detail activity

--- app/src/main/java/ch/ost/rj/mge/tasktracker/activities/DetailActivity.java
+++ app/src/main/java/ch/ost/rj/mge/tasktracker/activities/DetailActivity.java
@@ -6,11 +6,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import ch.ost.rj.mge.tasktracker.R;
+import ch.ost.rj.mge.tasktracker.fragments.DetailEffortFragment;
+import ch.ost.rj.mge.tasktracker.fragments.DetailStartFragment;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private DetailEffortFragment detailEffortFragment;
+    private DetailStartFragment detailStartFragment;
+
     public static Intent createIntent(Context context) {
-        Intent intent = new Intent(context, EditActivity.class);
+        Intent intent = new Intent(context, DetailActivity.class);
         return intent;
     }
 
@@ -19,6 +24,13 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        detailEffortFragment = DetailEffortFragment.create();
+        detailStartFragment = DetailStartFragment.create();
+        getSupportFragmentManager()
+            .beginTransaction()
+            .add(R.id.detail_effort_container, detailEffortFragment)
+            .add(R.id.detail_start_container, detailStartFragment)
+            .commit();
 
     }
 }
