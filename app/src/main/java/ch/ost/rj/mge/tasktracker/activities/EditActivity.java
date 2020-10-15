7efa37d3526a33d7970e1bf2510fb176635142f0
pemmeneg <philipp@emmenegger-mail.ch>
2020-10-15 12:49:42 +0200

    Started with Overview / Edit Activity

--- app/src/main/java/ch/ost/rj/mge/tasktracker/activities/EditActivity.java
+++ app/src/main/java/ch/ost/rj/mge/tasktracker/activities/EditActivity.java
@@ -2,10 +2,17 @@ package ch.ost.rj.mge.tasktracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import ch.ost.rj.mge.tasktracker.R;
 
 public class EditActivity extends AppCompatActivity {
+
+    public static Intent createIntent(Context context) {
+        Intent intent = new Intent(context, EditActivity.class);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ch/ost/rj/mge/tasktracker/activities/OverviewActivity.java
+++ app/src/main/java/ch/ost/rj/mge/tasktracker/activities/OverviewActivity.java
@@ -2,15 +2,32 @@ package ch.ost.rj.mge.tasktracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import ch.ost.rj.mge.tasktracker.R;
 
 public class OverviewActivity extends AppCompatActivity {
+    Button toEditBtn;
+    Button toDetailBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        toEditBtn = findViewById(R.id.toEditButton);
+        toDetailBtn = findViewById(R.id.toDetailButton);
+
+        toEditBtn.setOnClickListener(v -> {
+            Intent intent = EditActivity.createIntent(this);
+            startActivity(intent);
+        });
+
+        toDetailBtn.setOnClickListener(v -> {
+            Intent intent = DetailActivity.createIntent(this);
+            startActivity(intent);
+        });
     }
 }
